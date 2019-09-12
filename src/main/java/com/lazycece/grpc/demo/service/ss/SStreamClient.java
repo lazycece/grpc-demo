119272da6b2fbf9aaadae964649d8b6ba30dd5ec
lazycece <lazycece@gmail.com>
2019-09-12 11:11:44 +0800

    server stream

--- src/main/java/com/lazycece/grpc/demo/service/ss/SStreamClient.java
+++ src/main/java/com/lazycece/grpc/demo/service/ss/SStreamClient.java
@@ -1,8 +1,79 @@
 package com.lazycece.grpc.demo.service.ss;
 
+import com.lazycece.grpc.demo.proto.ss.HelloRequest;
+import com.lazycece.grpc.demo.proto.ss.HelloResponse;
+import com.lazycece.grpc.demo.proto.ss.SStreamServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @author lazycece
  * @date 2019/9/11
  */
 public class SStreamClient {
+
+    private static final Logger logger = Logger.getLogger(SStreamClient.class.getName());
+
+    private final ManagedChannel channel;
+    private final SStreamServiceGrpc.SStreamServiceStub stub;
+    private static final CountDownLatch COUNT_DOWN_LATCH = new CountDownLatch(1);
+
+    public SStreamClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext()
+                .build());
+    }
+
+    private SStreamClient(ManagedChannel channel) {
+        this.channel = channel;
+        stub = SStreamServiceGrpc.newStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public void sayHello(String name) {
+        logger.info("ready ...");
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        CustomStreamObserver streamObserver = new CustomStreamObserver();
+        stub.sayHello(request, streamObserver);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SStreamClient sStreamClient = new SStreamClient("localhost", 50051);
+        sStreamClient.sayHello("lazycece");
+        SStreamClient.COUNT_DOWN_LATCH.await();
+        sStreamClient.shutdown();
+    }
+
+    class CustomStreamObserver implements StreamObserver<HelloResponse> {
+
+        @Override
+        public void onNext(HelloResponse helloResponse) {
+            logger.info(helloResponse.getMessage());
+            COUNT_DOWN_LATCH.countDown();
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+            logger.log(Level.WARNING, throwable.getMessage());
+            COUNT_DOWN_LATCH.countDown();
+        }
+
+        @Override
+        public void onCompleted() {
+            logger.info("on completed");
+        }
+    }
+
+
 }
--- src/main/java/com/lazycece/grpc/demo/service/ss/SStreamServer.java
+++ src/main/java/com/lazycece/grpc/demo/service/ss/SStreamServer.java
@@ -1,8 +1,73 @@
 package com.lazycece.grpc.demo.service.ss;
 
+import com.lazycece.grpc.demo.proto.ss.HelloRequest;
+import com.lazycece.grpc.demo.proto.ss.HelloResponse;
+import com.lazycece.grpc.demo.proto.ss.SStreamServiceGrpc;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
 /**
  * @author lazycece
  * @date 2019/9/11
  */
 public class SStreamServer {
+
+    private static final Logger logger = Logger.getLogger(SStreamServer.class.getName());
+
+    private Server server;
+
+    private void start() throws IOException {
+        /* The port on which the server should run */
+        int port = 50051;
+        server = ServerBuilder.forPort(port)
+                .addService(new SStreamServiceImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                SStreamServer.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final SStreamServer sStreamServer = new SStreamServer();
+        sStreamServer.start();
+        sStreamServer.blockUntilShutdown();
+    }
+
+    static class SStreamServiceImpl extends SStreamServiceGrpc.SStreamServiceImplBase {
+        @Override
+        public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+
+            HelloResponse response = HelloResponse.newBuilder().setMessage("hello, " + request.getName()).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+    }
+
 }
